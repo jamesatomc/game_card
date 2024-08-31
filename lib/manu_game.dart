11a1a4220d19074714d31d0d17c486f9f02dfb4a
jamesatomc <jiraphonwansai2002@gmail.com>
2024-08-31 22:16:36 +0700

    Refactor game code to use GameCard from game1 instead of gamecard

--- lib/manu_game.dart
+++ lib/manu_game.dart
@@ -6,11 +6,11 @@ import 'package:flutter_cardgame/game2/GmaeJump.dart';
 import 'game_button.dart';
 
 class ManuGame extends StatefulWidget {
-  // ignore: use_super_parameters
-  const ManuGame({Key? key, required String username}) : super(key: key);
+  final String username;
+
+  const ManuGame({Key? key, required this.username}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _ManuGameState createState() => _ManuGameState();
 }
 
@@ -38,82 +38,99 @@ class _ManuGameState extends State<ManuGame> {
     const double _height = 64 - _shadowHeight1;
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: const DecorationImage(
-            image: AssetImage('assets/gamecard/one.gif'),
-            fit: BoxFit.cover,
-          ),
-          color: Colors.black.withOpacity(0.5), // Adjust opacity as needed
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GameButton(
-                height: _height,
-                shadowHeight: _shadowHeight1,
-                position: _position1,
-                text: 'Game 1',
-                onTap: () {
-                  _playSound();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const GameCardScreen()),
-                  );
-                },
-                onTapUp: () {
-                  setState(() {
-                    _position1 = 4;
-                  });
-                },
-                onTapDown: () {
-                  setState(() {
-                    _position1 = 0;
-                  });
-                },
-                onTapCancel: () {
-                  setState(() {
-                    _position1 = 4;
-                  });
-                },
-                backgroundColor: Colors.black,
-                animatedColor: Colors.blue,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: const DecorationImage(
+                image: AssetImage('assets/gamecard/one.gif'),
+                fit: BoxFit.cover,
               ),
-              GameButton(
-                height: _height,
-                shadowHeight: _shadowHeight2,
-                position: _position2,
-                text: 'Game 2',
-                onTap: () {
-                  _playSound();
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const GmaeJump()),
-                  );
-                },
-                onTapUp: () {
-                  setState(() {
-                    _position2 = 4;
-                  });
-                },
-                onTapDown: () {
-                  setState(() {
-                    _position2 = 0;
-                  });
-                },
-                onTapCancel: () {
-                  setState(() {
-                    _position2 = 4;
-                  });
-                },
-                backgroundColor: Colors.black,
-                animatedColor: const Color.fromARGB(255, 41, 45, 49),
+              color: Colors.black.withOpacity(0.5), // Adjust opacity as needed
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GameButton(
+                    height: _height,
+                    shadowHeight: _shadowHeight1,
+                    position: _position1,
+                    text: 'Game 1',
+                    onTap: () {
+                      _playSound();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GameCardScreen()),
+                      );
+                    },
+                    onTapUp: () {
+                      setState(() {
+                        _position1 = 4;
+                      });
+                    },
+                    onTapDown: () {
+                      setState(() {
+                        _position1 = 0;
+                      });
+                    },
+                    onTapCancel: () {
+                      setState(() {
+                        _position1 = 4;
+                      });
+                    },
+                    backgroundColor: Colors.black,
+                    animatedColor: Colors.blue,
+                  ),
+                  GameButton(
+                    height: _height,
+                    shadowHeight: _shadowHeight2,
+                    position: _position2,
+                    text: 'Game 2',
+                    onTap: () {
+                      _playSound();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const GmaeJump()),
+                      );
+                    },
+                    onTapUp: () {
+                      setState(() {
+                        _position2 = 4;
+                      });
+                    },
+                    onTapDown: () {
+                      setState(() {
+                        _position2 = 0;
+                      });
+                    },
+                    onTapCancel: () {
+                      setState(() {
+                        _position2 = 4;
+                      });
+                    },
+                    backgroundColor: Colors.black,
+                    animatedColor: const Color.fromARGB(255, 41, 45, 49),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: Text(
+              'Player: ${widget.username}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
