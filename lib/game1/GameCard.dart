df528bcd619fb3e67df96ae7a1fc9a45f3f8564c
jamesatomc <jiraphonwansai2002@gmail.com>
2024-09-01 23:53:22 +0700

    Refactor GameCard.dart to update background image path

--- lib/game1/GameCard.dart
+++ lib/game1/GameCard.dart
@@ -265,7 +265,7 @@ class _GameCardScreenState extends State<GameCardScreen> {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-                'assets/gamecard/bg_pixel3'), // Replace with your GIF path
+                'assets/gamecard/bg_pixel3.png'), // Replace with your GIF path
             fit: BoxFit.cover,
           ),
         ),
