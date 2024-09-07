d5ea150a73454db7136b336a4dc8cde643dff6ee
jamesatomc <jiraphonwansai2002@gmail.com>
2024-09-07 20:08:09 +0700

    Refactor Bumpy class in game-ui/bumpy.dart

--- lib/game2/components/game-ui/bumpy.dart
+++ lib/game2/components/game-ui/bumpy.dart
@@ -6,50 +6,48 @@ import 'package:flame_audio/flame_audio.dart';
 
 import 'player.dart';
 
-
-class Bumpy extends SpriteAnimationComponent with HasGameRef ,CollisionCallbacks {
+class Bumpy extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
   Bumpy({required super.position})
       : super(
             size: Vector2(40, 40), //ปรับขนาดตัวละคร
             anchor: Anchor.center); //กำหนดขนาด ใส่ค่าขนาด 2 ค่า
 
-      late SpriteAnimation action;
+  late SpriteAnimation action;
 
-      late SpriteAnimation idle;
-      late SpriteAnimation attack;
+  late SpriteAnimation idle;
+  late SpriteAnimation attack;
 
-      final bool isVertical = false;
-      final double offNeg = 10;
-      final double offPos = 10;
-      static const tilesize = 32;
-      double moveDirection = 1;
-      double moveSpeed = 50;
-      double rangeNeg = 0;
-      double rangePos = 0;
-
-
+  final bool isVertical = false;
+  final double offNeg = 10;
+  final double offPos = 10;
+  static const tilesize = 32;
+  double moveDirection = 1;
+  double moveSpeed = 50;
+  double rangeNeg = 0;
+  double rangePos = 0;
 
   @override
-  FutureOr<void> onLoad() async{
-
-     await loadAnimation().then((_) => {animation = action});
-    add(RectangleHitbox(collisionType: CollisionType.active,));
+  FutureOr<void> onLoad() async {
+    await loadAnimation().then((_) => {animation = action});
+    add(RectangleHitbox(
+      collisionType: CollisionType.active,
+    ));
 
     priority = -1;
     debugMode = true;
 
-    if(isVertical){
+    if (isVertical) {
       rangeNeg = position.y - offNeg * tilesize;
       rangePos = position.y + offPos * tilesize;
-    }else{
+    } else {
       rangeNeg = position.x - offNeg * tilesize;
       rangePos = position.x + offPos * tilesize;
     }
     return super.onLoad();
   }
 
-    Future<void> loadAnimation() async {
-   
+  Future<void> loadAnimation() async {
     idle = SpriteAnimation.fromFrameData(
       await gameRef.images.load("bumpyRun(16 x 16).png"),
       SpriteAnimationData.sequenced(
@@ -61,32 +59,30 @@ class Bumpy extends SpriteAnimationComponent with HasGameRef ,CollisionCallbacks
     action = idle;
   }
 
- 
-
   @override
   void update(double dt) {
     // position.x += moveDirection * moveSpeed * dt;
-    if(isVertical){
+    if (isVertical) {
       _moveVertically(dt);
-    }else{
+    } else {
       _moveHorizontally(dt);
     }
     super.update(dt);
   }
-  
+
   void _moveVertically(double dt) {
-    if(position.y >= rangePos){
+    if (position.y >= rangePos) {
       moveDirection = -1;
-    }else if(position.y <= rangeNeg){
+    } else if (position.y <= rangeNeg) {
       moveDirection = 1;
     }
     position.y += moveDirection * moveSpeed * dt;
-  }//monster เลื่อนขึ้นลง
-  
+  } //monster เลื่อนขึ้นลง
+
   void _moveHorizontally(double dt) {
-    if(position.x >= rangePos){
+    if (position.x >= rangePos) {
       moveDirection = -1;
-    }else if(position.x <= rangeNeg){
+    } else if (position.x <= rangeNeg) {
       moveDirection = 1;
     }
     position.x += moveDirection * moveSpeed * dt;
@@ -95,16 +91,15 @@ class Bumpy extends SpriteAnimationComponent with HasGameRef ,CollisionCallbacks
         (moveDirection > 0 && scale.x < 0)) {
       flipHorizontally();
     }
-  }//monster เดินไปมา
+  } //monster เดินไปมา
+
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    // TODO: implement onCollision
     super.onCollision(intersectionPoints, other);
-
-    if(other is Player){
+    if (other is Player) {
       FlameAudio.play("hit.wav");
-      removeFromParent();//ลบmonsterเมื่อโดนชน
+      other.removeFromParent(); // Remove the player from the game world
     }
   }
 }
